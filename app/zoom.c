8f1e56b5aaa354ff8e4e3b1cd1581691bacd850d
People doing a 16 bpc version of gimp <film@src.gnome.org>
2001-12-10 18:35:46 +0000

    pulldown menu seems to have disappeared, now it has returned

--- app/zoom.c
+++ app/zoom.c
@@ -4,9 +4,20 @@
 #include "zoom.h"
 #include "stdio.h"
 
+static int zoom_external_generated = 0;
+static int zoom_updating_ui = 0;
+static int zoom_updating_scale = 0;
+
+static void zoom_update_pulldown_slider(GDisplay *disp);
+static void zoom_update_slider(GDisplay *disp);
+static void zoom_update_pulldown(GDisplay *disp);
 static GDisplay *zoom_get_active_display();
-static void zoom_slider_value_change(
+static void zoom_slider_value_changed(
    GtkAdjustment *adjustment,
+   gpointer user_data);
+static void zoom_pulldown_value_changed(
+   GtkList *list,
+   GtkWidget *widget,
    gpointer user_data);
 
 ZoomControl * zoom_control = 0;
@@ -35,21 +46,50 @@ ZoomControl * zoom_control_open()
 
 void zoom_view_changed(GDisplay *disp)
 {
-    // first get the active display if there is none
-    if (!zoom_control)
-       return;
-    if (!zoom_control->gdisp) {
-       zoom_control->gdisp = zoom_get_active_display();
-    }
+   // ignore this event if we are updating scale, because that indicates
+   // that we caused the event to occur.
+   if (zoom_updating_scale)
+      return;
 
-    // if that failed, do nothing.
-    if (!zoom_control->gdisp) {
-       return;
-    }
+   // first get the active display if there is none
+   if (!zoom_control)
+      return;
+   if (!zoom_control->gdisp) {
+      zoom_control->gdisp = zoom_get_active_display();
+   }
 
-    // notify dialog's widgets to update zoom
-    
-    // notify drawing area to update rect.
+   // if that failed, do nothing.
+   if (!zoom_control->gdisp) {
+      return;
+   }
+
+   // indicate this event was generated externally
+   zoom_external_generated = 1;
+   
+   // notify dialog's widgets to update zoom
+   zoom_update_pulldown_slider(disp); 
+   // notify drawing area to update rect.
+   
+   // reset state
+   zoom_external_generated = 0;
+}
+
+void zoom_update_pulldown_slider(GDisplay *disp)
+{
+   zoom_update_pulldown(disp);
+   zoom_update_slider(disp);
+}
+
+void zoom_update_slider(GDisplay *disp)
+{
+   zoom_updating_ui = 1;
+   zoom_updating_ui = 0;
+}
+
+void zoom_update_pulldown(GDisplay *disp)
+{
+   zoom_updating_ui = 1;
+   zoom_updating_ui = 0;
 }
 
 ZoomControl * zoom_control_new()
@@ -79,11 +119,13 @@ ZoomControl * zoom_control_new()
   items = g_list_append (items, "8:1");
   items = g_list_append (items, "16:1");
   gtk_combo_set_popdown_strings (GTK_COMBO (zoom->pull_down), items);
+  gtk_signal_connect(GTK_OBJECT(GTK_COMBO(zoom->pull_down)->list), "select-child", 
+                  GTK_SIGNAL_FUNC(zoom_pulldown_value_changed), 0);
 
   zoom->adjust = GTK_ADJUSTMENT(gtk_adjustment_new(0, -4, 4, 1, -1, 1));
   zoom->slider = gtk_hscrollbar_new(zoom->adjust);
   gtk_signal_connect(GTK_OBJECT(zoom->adjust), "value-changed", 
-                  GTK_SIGNAL_FUNC(zoom_slider_value_change), 0);
+                  GTK_SIGNAL_FUNC(zoom_slider_value_changed), 0);
 
   zoom->drawing_area = gtk_drawing_area_new();
   gtk_drawing_area_size(GTK_DRAWING_AREA(zoom->drawing_area), 128, 128);
@@ -124,7 +166,7 @@ zoom_get_active_display()
 
 // called when the value of the zoom slider changes
 static void 
-zoom_slider_value_change(
+zoom_slider_value_changed(
    GtkAdjustment *adjustment,
    gpointer user_data)
 {
@@ -150,5 +192,30 @@ zoom_slider_value_change(
 
     // now set the zoom factor for the display
     change_scale(zoom_control->gdisp, scale_val); 
+}
+
+static void zoom_pulldown_value_changed(
+   GtkList *list,
+   GtkWidget *widget,
+   gpointer user_data)
+{
+   int scale_val;
+   gchar * text;
+
+   if (!widget)
+      return;
+
+   // first get the active display if there is none
+   if (!zoom_control->gdisp) {
+      zoom_control->gdisp = zoom_get_active_display();
+   }
+
+   // if that failed, do nothing.
+   if (!zoom_control->gdisp) {
+      return;
+   }
+
+   gtk_label_get(GTK_LABEL(GTK_BIN(widget)->child), &text);
+   printf("label: %s\n", text);
 }
 
