8772ecb0de4a82da4ff1af8b5a7ed71027996cce
Michael Natterer <mitch@gimp.org>
2018-04-29 02:53:51 +0200

    Get rid of the deprecated gtk_icon_size_lookup_for_settings()
    and use gtk_icon_size_lookup() instead.

--- app/widgets/gimpcontainerbox.c
+++ app/widgets/gimpcontainerbox.c
@@ -202,8 +202,7 @@ gimp_container_box_get_preview (GimpDocked   *docked,
 
   g_return_val_if_fail (container != NULL, NULL);
 
-  gtk_icon_size_lookup_for_settings (gtk_widget_get_settings (GTK_WIDGET (box)),
-                                     size, &width, &height);
+  gtk_icon_size_lookup (size, &width, &height);
 
   prop_name = gimp_context_type_to_prop_name (gimp_container_get_children_type (container));
 
